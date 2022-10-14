867397375a276d7f1d8f09ca772c951cbfaea278
pc20211187 <115199239+pc20211187@users.noreply.github.com>
2022-10-14 09:30:46 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 let counter = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(randint(1, 6))
+    counter = -1
 })
 input.onButtonPressed(Button.B, function () {
     counter = 9
-    for (let index = 0; index < 10; index++) {
+    while (counter >= 0) {
         basic.showString("" + (counter))
         counter += -1
     }
